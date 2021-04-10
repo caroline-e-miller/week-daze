1cfd190eca0bc47c7845f7e9ec491c2f2e74fb22
caroline-e-miller <caroline.miller@ymail.com>
2021-04-10 12:55:48 -0500

    Revert "fixed wording"
    This reverts commit f37ca89602c93196d42a052dd4adaea2c3706f42.

--- controllers/api/jobRoutes.js
+++ controllers/api/jobRoutes.js
@@ -7,7 +7,7 @@ router.post('/', async (req, res) => {
         const jobData = await Job.create(req.body);
 
         req.session.save(() => {
-            req.session.user_id = jobData.id;
+            req.session.user_id = userData.id;
             req.session.logged_in = true;
 
             res.status(200).json(jobData);
