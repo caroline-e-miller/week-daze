2593d3a58c1afbe1d0936e1517989fbab2cb042c
caroline-e-miller <caroline.miller@ymail.com>
2021-04-10 12:33:29 -0500

    i dont know :(

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const jobRoutes = require('./jobRoutes');
 
-router.use('/users', userRoutes);
+// router.use('/users', userRoutes);
 router.use('/jobs', jobRoutes);
-
+console.log(router);
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+console.log(apiRoutes);
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
