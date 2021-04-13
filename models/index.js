60d45698e602b57f77ecc957fbf1d171f9966a20
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-04-13 09:53:56 -0500

    created status model and data to seed

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Job = require('./Job');
+const Status = require('./Status');
 
 User.hasMany(Job, {
     foreignKey: 'user_id',
@@ -9,4 +10,12 @@ Job.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = { User, Job };
+Status.belongsTo(Job, {
+    foreignKey: 'job_id'
+});
+
+Job.hasMany(Status, {
+    foreignKey:"job_id"
+});
+
+module.exports = { User, Job, Status };
